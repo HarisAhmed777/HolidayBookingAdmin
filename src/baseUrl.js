265c97d677a4f4c311579216516cbf67fc761343
HarisAhmed777 <ahmedharis371@gmail.com>
2024-12-13 23:39:56 +0530

    first commit

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,5 +1,5 @@
-   export const baseUrl = 'http://localhost:3333'
+   // export const baseUrl = 'http://localhost:3333'
    // export const baseUrl = 'https://sandbox.waawtech.com:3333'
-   // export  const baseUrl = 'https://demoadminbackendnrender.com'
+   export  const baseUrl = 'https://demoadminbackendnrender.com'
    //
    
